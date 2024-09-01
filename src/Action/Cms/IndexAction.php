3fbabf4e4b14f4f1263d580273b48c7269a62b2b
wolxXx <devops@wolxXx.de>
2024-09-01 12:11:10 +0200

    Update task retrieval method in IndexAction
    Changed the task retrieval method in IndexAction to use findForIndex instead of findAll. This ensures that only tasks relevant for the index view are fetched, potentially improving performance and relevance of the displayed data.

--- src/Action/Cms/IndexAction.php
+++ src/Action/Cms/IndexAction.php
@@ -21,7 +21,7 @@ class IndexAction extends \DoEveryApp\Action\AbstractAction
 
         return $this->render('action/cms/dashboard', [
             'executions'        => \DoEveryApp\Entity\Execution::getRepository()->findForIndex(),
-            'tasks'             => \DoEveryApp\Entity\Task::getRepository()->findAll(),
+            'tasks'             => \DoEveryApp\Entity\Task::getRepository()->findForIndex(),
             'tasksWithoutGroup' => \DoEveryApp\Entity\Task::getRepository()->getWithoutGroup(),
             'groups'            => \DoEveryApp\Entity\Group::getRepository()->findAll(),
             'workers'           => \DoEveryApp\Entity\Worker::getRepository()->findAll(),
