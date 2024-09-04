92aa6b3db154347acafbbb5de069412fb6903364
wolxXx <devops@wolxXx.de>
2024-09-04 08:58:40 +0200

    Add setViewHeaders script for uniform view headers
    Introduce a script `setViewHeaders.php` to standardize headers for view files. The script processes files in the `src/views` directory, ensuring they start with a consistent header declaration and variable definitions. This helps maintain code consistency across the project.

---
+++ scripts/setViewHeaders.php
@@ -0,0 +1,47 @@
+<?php
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+$viewPaths = [];
+
+$newHeader = <<<PHP
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @var \$this                \Slim\Views\PhpRenderer
+ * @var \$errorStore          \DoEveryApp\Util\ErrorStore
+ * @var \$currentRoute        string
+ * @var \$currentRoutePattern string
+ * @var \$currentUser         \DoEveryApp\Entity\Worker|null
+PHP;
+
+$directoryIterator = new \RecursiveDirectoryIterator(ROOT_DIR . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'views');
+$iterator          = new \RecursiveIteratorIterator($directoryIterator);
+$regexIterator     = new \RegexIterator($iterator, '/^.+\.php$/i', \RecursiveRegexIterator::GET_MATCH);
+foreach ($regexIterator as $current) {
+    $file        = $current[0];
+    $viewPaths[] = realpath($file);
+}
+foreach ($viewPaths as $path) {
+    $content = file_get_contents($path);
+    $until   = ' */';
+    while (true) {
+        $firstLine = explode("\n", $content, 2)[0];
+        $content   = substr($content, strpos($content, "\n") + 1);
+        if ($until === $firstLine) {
+            break;
+        }
+        if ('' === $content) {
+            \DoEveryApp\Util\Debugger::debug($path);
+            //something went really really wrong xD
+            die('OMFG!!!');
+        }
+    }
+
+    $content = $newHeader . PHP_EOL . $until . PHP_EOL . $content;
+
+
+    file_put_contents($path, $content);
+}
