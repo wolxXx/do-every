6435df011fcac59bf56a92e0481a00454abb2637
wolxXx <devops@wolxXx.de>
2024-09-04 08:33:17 +0200

    Add password change check function in Worker utility class
    Introduced the `isTimeForPasswordChange` method to determine if a worker's password needs updating. This method checks if the worker has a password and when it was last changed, comparing it to a three-month threshold.

--- src/Util/View/Worker.php
+++ src/Util/View/Worker.php
@@ -15,4 +15,17 @@ class Worker
         return \DoEveryApp\Util\View\Escaper::escape($worker->getName());
     }
 
+
+    public static function isTimeForPasswortChange(\DoEveryApp\Entity\Worker $worker): bool
+    {
+        if (null === $worker->getPassword()) {
+            return false;
+        }
+        if (null === $worker->getLastPasswordChange()) {
+            return true;
+        }
+        $due = \Carbon\Carbon::create($worker->getLastPasswordChange())->addMonths(3);
+
+        return \Carbon\Carbon::now()->greaterThan($due);
+    }
 }
