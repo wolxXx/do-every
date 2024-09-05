244d7dc84203b0ddbee6fb7108c744ef8a22f012
wolxXx <devops@wolxXx.de>
2024-09-05 19:06:03 +0200

    Refactor cron script to use new Cron utility
    Replaced individual logging and echo statements with a single instantiation of the new Cron utility from the \DoEveryApp\Util namespace. This change simplifies the script and ensures consistent behavior.

--- scripts/cron.php
+++ scripts/cron.php
@@ -2,13 +2,4 @@
 
 require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-\DoEveryApp\Util\DependencyContainer::getInstance()
-    ->getLogger()
-    ->debug('bar');
-
-echo "foo";
-echo "foo";
-echo "foo";
-echo "foo";
-
-\DoEveryApp\Util\Debugger::debug('asdf');
+new \DoEveryApp\Util\Cron();
