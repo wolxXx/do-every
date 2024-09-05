048ea29739a9670fa2b009d0d6951e91c1a9a12d
wolxXx <devops@wolxXx.de>
2024-09-05 07:58:30 +0200

    Add Registry entity
    Introduced a new `Registry` entity with various fields and associations. Includes mapped columns for different data types and relationships with Execution, Group, Task, and Worker entities to ensure proper database interactions.

---
+++ src/Entity/Registry.php
@@ -0,0 +1,333 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoEveryApp\Entity;
+
+#[\Doctrine\ORM\Mapping\Entity(
+    repositoryClass: Registry\Repository::class
+)]
+#[\Doctrine\ORM\Mapping\Table(
+    name   : self::TABLE_NAME,
+    options: [
+        'collate' => 'utf8_general_ci',
+        'charset' => 'utf8',
+        'engine'  => 'InnoDB',
+    ],
+)]
+#[\Doctrine\ORM\Mapping\UniqueConstraint(
+    name   : 'key',
+    columns: ['key']
+)]
+class Registry
+{
+    use \DoEveryApp\Entity\Share\Blame;
+    use \DoEveryApp\Entity\Share\Id;
+    use \DoEveryApp\Entity\Share\Repository;
+    use \DoEveryApp\Entity\Share\Timestamp;
+
+    public const string TABLE_NAME = 'registry';
+
+    #[\Doctrine\ORM\Mapping\Column(
+        name    : 'key',
+        type    : \Doctrine\DBAL\Types\Types::STRING,
+        nullable: false
+    )]
+    protected string     $key;
+
+    #[\Doctrine\ORM\Mapping\Column(
+        name    : 'int_value',
+        type    : \Doctrine\DBAL\Types\Types::INTEGER,
+        nullable: true
+    )]
+    protected ?int       $intValue         = null;
+
+    #[\Doctrine\ORM\Mapping\Column(
+        name    : 'bool_value',
+        type    : \Doctrine\DBAL\Types\Types::BOOLEAN,
+        nullable: true
+    )]
+    protected ?bool      $boolValue        = null;
+
+    #[\Doctrine\ORM\Mapping\Column(
+        name    : 'string_value',
+        type    : \Doctrine\DBAL\Types\Types::STRING,
+        nullable: true
+    )]
+    protected ?string    $stringValue      = null;
+
+    #[\Doctrine\ORM\Mapping\Column(
+        name    : 'date_value',
+        type    : \Doctrine\DBAL\Types\Types::DATETIME_MUTABLE,
+        nullable: true
+    )]
+    protected ?\DateTime $dateValue        = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Execution::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'execution_reference_set_null',
+        nullable: true,
+        onDelete: 'SET NULL'
+    )]
+    protected ?Execution $executionSetNull = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Execution::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'execution_reference_cascade',
+        nullable: true,
+        onDelete: 'CASCADE'
+    )]
+    protected ?Execution $executionCascade = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Group::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'group_reference_set_null',
+        nullable: true,
+        onDelete: 'SET NULL'
+    )]
+    protected ?Group     $groupSetNull     = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Group::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'group_reference_cascade',
+        nullable: true,
+        onDelete: 'CASCADE'
+    )]
+    protected ?Group     $groupCascade     = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Task::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'task_reference_set_null',
+        nullable: true,
+        onDelete: 'SET NULL'
+    )]
+    protected ?Task      $taskSetNull      = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Task::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'task_reference_cascade',
+        nullable: true,
+        onDelete: 'CASCADE'
+    )]
+    protected ?Task      $taskCascade      = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Worker::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'worker_reference_set_null',
+        nullable: true,
+        onDelete: 'SET NULL'
+    )]
+    protected ?Worker    $workerSetNull    = null;
+
+    #[\Doctrine\ORM\Mapping\ManyToOne(
+        targetEntity: Worker::class,
+    )]
+    #[\Doctrine\ORM\Mapping\JoinColumn(
+        name    : 'worker_reference_cascade',
+        nullable: true,
+        onDelete: 'CASCADE'
+    )]
+    protected ?Worker    $workerCascade    = null;
+
+
+    public static function getRepository(): Registry\Repository
+    {
+        return static::getRepositoryByClassName();
+    }
+
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+
+    public function setKey(string $key): static
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+
+    public function getIntValue(): ?int
+    {
+        return $this->intValue;
+    }
+
+
+    public function setIntValue(?int $intValue): static
+    {
+        $this->intValue = $intValue;
+
+        return $this;
+    }
+
+
+    public function getBoolValue(): ?bool
+    {
+        return $this->boolValue;
+    }
+
+
+    public function setBoolValue(?bool $boolValue): static
+    {
+        $this->boolValue = $boolValue;
+
+        return $this;
+    }
+
+
+    public function getStringValue(): ?string
+    {
+        return $this->stringValue;
+    }
+
+
+    public function setStringValue(?string $stringValue): static
+    {
+        $this->stringValue = $stringValue;
+
+        return $this;
+    }
+
+
+    public function getDateValue(): ?\DateTime
+    {
+        return $this->dateValue;
+    }
+
+
+    public function setDateValue(?\DateTime $dateValue): static
+    {
+        $this->dateValue = $dateValue;
+
+        return $this;
+    }
+
+
+    public function getExecutionSetNull(): ?Execution
+    {
+        return $this->executionSetNull;
+    }
+
+
+    public function setExecutionSetNull(?Execution $executionSetNull): static
+    {
+        $this->executionSetNull = $executionSetNull;
+
+        return $this;
+    }
+
+
+    public function getExecutionCascade(): ?Execution
+    {
+        return $this->executionCascade;
+    }
+
+
+    public function setExecutionCascade(?Execution $executionCascade): static
+    {
+        $this->executionCascade = $executionCascade;
+
+        return $this;
+    }
+
+
+    public function getGroupSetNull(): ?Group
+    {
+        return $this->groupSetNull;
+    }
+
+
+    public function setGroupSetNull(?Group $groupSetNull): static
+    {
+        $this->groupSetNull = $groupSetNull;
+
+        return $this;
+    }
+
+
+    public function getGroupCascade(): ?Group
+    {
+        return $this->groupCascade;
+    }
+
+
+    public function setGroupCascade(?Group $groupCascade): static
+    {
+        $this->groupCascade = $groupCascade;
+
+        return $this;
+    }
+
+
+    public function getTaskSetNull(): ?Task
+    {
+        return $this->taskSetNull;
+    }
+
+
+    public function setTaskSetNull(?Task $taskSetNull): static
+    {
+        $this->taskSetNull = $taskSetNull;
+
+        return $this;
+    }
+
+
+    public function getTaskCascade(): ?Task
+    {
+        return $this->taskCascade;
+    }
+
+
+    public function setTaskCascade(?Task $taskCascade): static
+    {
+        $this->taskCascade = $taskCascade;
+
+        return $this;
+    }
+
+
+    public function getWorkerSetNull(): ?Worker
+    {
+        return $this->workerSetNull;
+    }
+
+
+    public function setWorkerSetNull(?Worker $workerSetNull): static
+    {
+        $this->workerSetNull = $workerSetNull;
+
+        return $this;
+    }
+
+
+    public function getWorkerCascade(): ?Worker
+    {
+        return $this->workerCascade;
+    }
+
+
+    public function setWorkerCascade(?Worker $workerCascade): static
+    {
+        $this->workerCascade = $workerCascade;
+
+        return $this;
+    }
+}
