04b339edd63c1d88320a230930322005ee0d79a1
wolxXx <devops@wolxXx.de>
2024-09-06 09:23:24 +0200

    Add Backup class for periodic database dumps
    Introduce a Backup class in the Util/Cron namespace to automate database backups every 2 hours. The class checks the time of the last backup, creates a new backup file using `mysqldump`, and updates the last backup timestamp in the registry.

---
+++ src/Util/Cron/Backup.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoEveryApp\Util\Cron;
+
+class Backup
+{
+    public function __construct()
+    {
+        $now     = \Carbon\Carbon::now();
+        $lastRun = \DoEveryApp\Util\Registry::getInstance()
+                                            ->getLastBackup()
+        ;
+        if (null !== $lastRun) {
+            $lastRun = \Carbon\Carbon::create($lastRun);
+            $lastRun->addHours(2);
+            if ($lastRun->gt($now)) {
+                return;
+            }
+        }
+
+        $dbParams = require \ROOT_DIR . \DIRECTORY_SEPARATOR . 'doctrineConfiguration.php';
+        $command  = 'mysqldump --column-statistics=0 -u%s -p%s -P%s -h%s --triggers --routines --single-transaction %s > %s';
+
+        $now = \Carbon\Carbon::now();
+
+        $path = \ROOT_DIR . \DIRECTORY_SEPARATOR . 'backups' . \DIRECTORY_SEPARATOR . $now->year . DIRECTORY_SEPARATOR . $now->month . DIRECTORY_SEPARATOR . $now->day . \DIRECTORY_SEPARATOR;
+        if (false === \is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+        $path    = $path . 'backup_' . date('Y-m-d_H-i-s') . '.sql';
+        $command = sprintf($command, $dbParams['user'], $dbParams['password'], $dbParams['port'], $dbParams['host'], $dbParams['dbname'], $path);
+
+        exec($command);
+
+
+        \DoEveryApp\Util\Registry::getInstance()
+                                 ->setLastBackup(\Carbon\Carbon::now())
+        ;
+    }
+}
