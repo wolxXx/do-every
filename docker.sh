ff11852d4f258fc7e30edb3de1f87fbb8407bf5b
wolxXx <devops@wolxXx.de>
2024-09-08 23:17:10 +0200

    Rename and template docker-compose file
    Renamed docker-compose.yml to docker-compose.yml.dist and parameterized placeholder values (__INSTANCE__, __DB_PORT__, __WEB_PORT__). Added a script (docker.sh) to generate docker-compose.yml from the template using values from the .env file. Updated .gitignore to exclude the generated docker-compose.yml file.

---
+++ docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+HERE=$(dirname $(readlink -f "$0"));
+cd "$HERE";
+
+source .env
+cp ./docker-compose.yml.dist ./docker-compose.yml
+sed -i -e "s/__INSTANCE__/${INSTANCE}/g" ./docker-compose.yml
+sed -i -e "s/__DB_PORT__/${DB_PORT}/g" ./docker-compose.yml
+sed -i -e "s/__WEB_PORT__/${WEB_PORT}/g" ./docker-compose.yml
+
+exit 0;
