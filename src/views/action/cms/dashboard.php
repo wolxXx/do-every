99790f1baa133f1fc771f48bfe1d16c371cd55bf
wolxXx <devops@wolxXx.de>
2024-09-08 20:29:59 +0200

    Fix interval type null check in dashboard task list
    Ensure `getElapsingTypeByTask` is called only if `getIntervalType` is not null. This prevents potential errors when displaying task intervals.

--- src/views/action/cms/dashboard.php
+++ src/views/action/cms/dashboard.php
@@ -155,7 +155,9 @@ $durations = \DoEveryApp\Definition\Durations::FactoryForGlobal();
                 </td>
                 <td>
                     <?= \DoEveryApp\Util\View\IntervalHelper::get($task) ?>
-                    (<?= \DoEveryApp\Util\View\IntervalHelper::getElapsingTypeByTask($task) ?>)
+                    <? if(null !== $task->getIntervalType()): ?>
+                        (<?= \DoEveryApp\Util\View\IntervalHelper::getElapsingTypeByTask($task) ?>)
+                    <? endif ?>
                 </td>
                 <td>
                     <a href="<?= \DoEveryApp\Action\Task\ShowAction::getRoute($task->getId()) ?>">
