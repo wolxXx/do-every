602451b24a4a062eea84ae6d7b731fc9dd573547
wolxXx <devops@wolxXx.de>
2024-09-08 20:30:08 +0200

    Enhance CMS debug view with additional system details
    Added table rows to display detailed information about the system's state, including admin user, cron status, timestamps, and various limits. This improvement provides more comprehensive insight for debugging purposes.

--- src/views/action/cms/debug.php
+++ src/views/action/cms/debug.php
@@ -21,21 +21,112 @@ declare(strict_types=1);
 
     </thead>
     <tbody>
+
+        <tr>
+            <td>
+                super admin admin :<br />
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\Worker::get(\DoEveryApp\Util\Registry::getInstance()->getAdminUser()) ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                cron läuft:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\Boolean::get(\DoEveryApp\Util\Registry::getInstance()->isCronRunning()) ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                cron gestartet:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\DateTime::getDateTimeMediumDateMediumTime(\DoEveryApp\Util\Registry::getInstance()->getCronStarted()) ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                letzte cron ausführung:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\DateTime::getDateTimeMediumDateMediumTime(\DoEveryApp\Util\Registry::getInstance()->getLastCron()) ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                letztes backup:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\DateTime::getDateTimeMediumDateMediumTime(\DoEveryApp\Util\Registry::getInstance()->getLastBackup()) ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Fälligkeitpräzision:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\Registry::getInstance()->getPrecisionDue() ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Zeitlinie auffüllen:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\Boolean::get(\DoEveryApp\Util\Registry::getInstance()->doFillTimeLine()) ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                maximale Anzahl Worker:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\Escaper::escape(\DoEveryApp\Util\Registry::getInstance()->getMaxWorkers(), '-') ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                maximale Anzahl Aufgaben:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\Escaper::escape(\DoEveryApp\Util\Registry::getInstance()->getMaxTasks(), '-') ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                maximale Anzahl Gruppen:
+            </td>
+            <td>
+                <?= \DoEveryApp\Util\View\Escaper::escape(\DoEveryApp\Util\Registry::getInstance()->getMaxGroups(), '-') ?>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<table>
+    <thead>
+        <? foreach(\DoEveryApp\Entity\Registry::getRepository()->findAll() as $entry): ?>
+            <tr>
+                <? foreach((array) $entry as $key => $value): ?>
+                    <th>
+                        <?= \DoEveryApp\Util\View\Escaper::escape($key) ?>
+                    </th>
+                <? endforeach ?>
+            </tr>
+            <? break ?>
+        <? endforeach ?>
+    </thead>
+    <tbody>
         <? foreach(\DoEveryApp\Entity\Registry::getRepository()->findAll() as $entry): ?>
             <tr>
                 <? foreach((array) $entry as $key => $value): ?>
                     <td>
-                        <?= \DoEveryApp\Util\View\Escaper::escape($key) ?>:
                         <?= \DoEveryApp\Util\View\DisplayValue::do($value) ?>
-                        <? #\DoEveryApp\Util\Debugger::debug($value); ?>
                     </td>
                 <? endforeach ?>
             </tr>
         <? endforeach ?>
-        <tr>
-            <td>
-
-            </td>
-        </tr>
     </tbody>
 </table>
