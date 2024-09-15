d1a387c32a432d52d8947f8c4f64caa532b0ddf3
wolxXx <devops@wolxXx.de>
2024-09-15 17:09:12 +0200

    Move doctrineBootstrap inclusion below ROOT_DIR definition
    Rearranged the script to define ROOT_DIR before including doctrineBootstrap.php. This ensures that all relative paths are correctly set up before any other dependencies are loaded.

--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'doctrineBootstrap.php';
 chdir(dirname(__DIR__));
 defined('ROOT_DIR') || define('ROOT_DIR', getcwd());
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'doctrineBootstrap.php';
