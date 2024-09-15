fc419c30f4b46c264db6944abf1acd8ee635f904
wolxXx <devops@wolxXx.de>
2024-09-15 20:28:43 +0200

    Add data providers and new test cases for Translator
    Introduced data providers to supply test methods for Translator classes. Added test cases to validate different translations (Nothing, German, English) and their instantiation. Removed redundant reflection-based method calls.

--- test/Util/TranslatorTest.php
+++ test/Util/TranslatorTest.php
@@ -7,16 +7,46 @@ namespace DoEveryAppTest\Util;
 
 class TranslatorTest extends \DoEveryAppTest\TestBase
 {
+    #[\PHPUnit\Framework\Attributes\DataProvider('translationsTestDataProvider')]
+    public function testNothingTranslations($method)
+    {
+        (new \DoEveryApp\Util\Translator\Nothing())->{$method}();
+        $this->assertTrue(true);
+    }
+    #[\PHPUnit\Framework\Attributes\DataProvider('translationsTestDataProvider')]
+    public function testGermanTranslations($method)
+    {
+        (new \DoEveryApp\Util\Translator\German())->{$method}();
+        $this->assertTrue(true);
+    }
+
+    #[\PHPUnit\Framework\Attributes\DataProvider('translationsTestDataProvider')]
+    public function testEnglishTranslations($method)
+    {
+        (new \DoEveryApp\Util\Translator\English())->{$method}();
+        $this->assertTrue(true);
+    }
+
+
+    public static function translationsTestDataProvider()
+    {
+        $methods           = [];
+        $reflection        = new \ReflectionClass(\DoEveryApp\Util\Translator::class);
+        $reflectionMethods = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
+        foreach ($reflectionMethods as $method) {
+            $methods[] = [$method->getName()];
+        }
+
+        return $methods;
+    }
+
+
     public function testGermanInstantiation()
     {
         $translator = new \DoEveryApp\Util\Translator\German();
         $this->assertInstanceOf(\DoEveryApp\Util\Translator::class, $translator);
         $this->assertInstanceOf(\DoEveryApp\Util\Translator\German::class, $translator);
         $this->assertSame('Dashboard', ($translator)->dashboard());
-        $reflection = new \ReflectionClass($translator);
-        foreach ($reflection->getMethods() as $method) {
-            $translator->{$method->getName()}();
-        }
     }
 
 
@@ -26,9 +56,14 @@ class TranslatorTest extends \DoEveryAppTest\TestBase
         $this->assertInstanceOf(\DoEveryApp\Util\Translator::class, $translator);
         $this->assertInstanceOf(\DoEveryApp\Util\Translator\English::class, $translator);
         $this->assertSame('dashboard', ($translator)->dashboard());
-        $reflection = new \ReflectionClass($translator);
-        foreach ($reflection->getMethods() as $method) {
-            $translator->{$method->getName()}();
-        }
+    }
+
+
+    public function testNothingInstantiation()
+    {
+        $translator = new \DoEveryApp\Util\Translator\Nothing();
+        $this->assertInstanceOf(\DoEveryApp\Util\Translator::class, $translator);
+        $this->assertInstanceOf(\DoEveryApp\Util\Translator\Nothing::class, $translator);
+        $this->assertSame('..', ($translator)->dashboard());
     }
 }
