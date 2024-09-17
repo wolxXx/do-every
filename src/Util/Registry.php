8c4f416e13a1a19480421dce7f347a907182c38c
wolxXx <devops@wolxXx.de>
2024-09-17 09:31:57 +0200

    Make registry keys public and add notifier methods
    Converted private constants to public for easier access. Added new methods to handle notifier running state and last run datetime. Cleaned up some formatting inconsistencies.

--- src/Util/Registry.php
+++ src/Util/Registry.php
@@ -6,31 +6,35 @@ namespace DoEveryApp\Util;
 
 final class Registry
 {
-    private const string KEY_ADMIN_USER       = '7e0b853b-7e45-4e17-9458-89803fcd2c1e';
+    public const string KEY_ADMIN_USER        = '7e0b853b-7e45-4e17-9458-89803fcd2c1e';
 
-    private const string KEY_CRON_LOCK        = '84c29c0a-cbd9-4bc6-8532-feb785f4c59d';
+    public const string KEY_CRON_LOCK         = '84c29c0a-cbd9-4bc6-8532-feb785f4c59d';
 
-    private const string KEY_CRON_STARTED     = '05226812-dcf8-4e36-89ac-1dccc3e06047';
+    public const string KEY_CRON_STARTED      = '05226812-dcf8-4e36-89ac-1dccc3e06047';
 
-    private const string KEY_FILL_TIME_LINE   = '49a4421b-b6f4-458e-a7cd-5253a78305db';
+    public const string KEY_FILL_TIME_LINE    = '49a4421b-b6f4-458e-a7cd-5253a78305db';
 
-    private const string KEY_KEEP_BACKUP_DAYS = 'fbc976e8-629c-4f49-b17c-88a82482def3';
+    public const string KEY_KEEP_BACKUP_DAYS  = 'fbc976e8-629c-4f49-b17c-88a82482def3';
 
-    private const string KEY_LAST_BACKUP      = 'e16ede03-9703-4ce3-a075-88d4a64706cb';
+    public const string KEY_LAST_BACKUP       = 'e16ede03-9703-4ce3-a075-88d4a64706cb';
 
-    private const string KEY_LAST_CRON        = '449b1579-5540-4d06-b076-dfcfea73ff3c';
+    public const string KEY_LAST_CRON         = '449b1579-5540-4d06-b076-dfcfea73ff3c';
 
-    private const string KEY_MAX_GROUPS       = 'e15e9173-2776-4848-9419-0dfc0112db62';
+    public const string KEY_MAX_GROUPS        = 'e15e9173-2776-4848-9419-0dfc0112db62';
 
-    private const string KEY_MAX_TASKS        = 'd5a3211d-7e3f-4db8-98f0-339036409289';
+    public const string KEY_MAX_TASKS         = 'd5a3211d-7e3f-4db8-98f0-339036409289';
 
-    private const string KEY_MAX_WORKERS      = '0e18481e-767b-41c7-b74a-31b4ffc6bc01';
+    public const string KEY_MAX_WORKERS       = '0e18481e-767b-41c7-b74a-31b4ffc6bc01';
 
-    private const string KEY_PRECISION_DUE    = '902069d4-7b4a-4c04-9af5-0d1432ac105d';
+    public const string KEY_PRECISION_DUE     = '902069d4-7b4a-4c04-9af5-0d1432ac105d';
+
+    public const string KEY_NOTIFIER_RUNNING  = 'bd0f21dd-da3c-4986-b660-51c47edf6eeb';
+
+    public const string KEY_NOTIFIER_LAST_RUN = '4cf10630-b664-43cd-9693-0effe0934844';
 
     private static Registry $instance;
 
-    private array $cache = [];
+    private array           $cache = [];
 
 
     public static function getInstance(): static
@@ -41,6 +45,7 @@ final class Registry
 
         return static::$instance;
     }
+
 
     private final function __construct()
     {
@@ -55,6 +60,7 @@ final class Registry
         if (false === array_key_exists($key, $this->cache)) {
             return null;
         }
+
         return $this->cache[$key];
     }
 
@@ -170,16 +176,53 @@ final class Registry
     }
 
 
+    public function isNotifierRunning(): bool
+    {
+        return true === $this
+                ->getRow(self::KEY_NOTIFIER_RUNNING)
+                ?->getBoolValue()
+        ;
+    }
+
+
+    public function setNotifierRunning(bool $notifierRunning): static
+    {
+        return $this->updateRow(
+            $this
+                ->getOrCreateRow(self::KEY_NOTIFIER_RUNNING)
+                ->setBoolValue($notifierRunning)
+        );
+    }
+
+
+    public function getNotifierLastRun(): ?\DateTime
+    {
+        return $this
+            ->getRow(self::KEY_NOTIFIER_LAST_RUN)
+            ?->getDateValue()
+        ;
+    }
+
+
+    public function setNotifierLastRun(\DateTime $notifierLastRun): static
+    {
+        return $this->updateRow(
+            $this
+                ->getOrCreateRow(self::KEY_NOTIFIER_LAST_RUN)
+                ->setDateValue($notifierLastRun)
+        );
+    }
+
+
     #endregion crons
 
 
     public function getKeepBackupDays(): int
     {
         return $this
-            ->getRow(self::KEY_KEEP_BACKUP_DAYS)
-            ?->getIntValue()
-            ?? 30
-        ;
+                   ->getRow(self::KEY_KEEP_BACKUP_DAYS)
+                   ?->getIntValue()
+               ?? 30;
     }
 
 
@@ -191,6 +234,7 @@ final class Registry
                 ->setIntValue($days)
         );
     }
+
 
     public function getLastBackup(): ?\DateTime
     {
@@ -271,9 +315,9 @@ final class Registry
     public function getPrecisionDue(): int
     {
         return $this
-            ->getRow(self::KEY_PRECISION_DUE)
-            ?->getIntValue()
-            ?? 3;
+                   ->getRow(self::KEY_PRECISION_DUE)
+                   ?->getIntValue()
+               ?? 3;
     }
 
 
