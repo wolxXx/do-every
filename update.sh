c9b65f7de5da07b6f125be54160118bb6ca48dd8
wolxXx <devops@wolxXx.de>
2024-09-18 07:52:57 +0200

    Update temp file removal path in update.sh script
    This change modifies the path for deleting temporary files in the `update.sh` script to target files prefixed with `__CG__` in the `/tmp` directory. This ensures that only specific cache files are removed during the update process.

--- update.sh
+++ update.sh
@@ -14,11 +14,11 @@ echo "dn port: $DB_PORT"
 
 git pull
 ./docker.sh
-docker exec -it do-every-$INSTANCE-php83-web bash -c "rm /tmp__*"
+docker exec -it do-every-$INSTANCE-php83-web bash -c "rm /tmp/__CG__*"
 docker exec -it do-every-$INSTANCE-php83-web bash -c "php composer.phar install"
 docker exec -it do-every-$INSTANCE-php83-web bash -c "./install.sh"
 docker exec -it do-every-$INSTANCE-php83-web bash -c "php composer.phar dbFull"
-docker exec -it do-every-$INSTANCE-php83-web bash -c "rm /tmp__*"
+docker exec -it do-every-$INSTANCE-php83-web bash -c "rm /tmp/__CG__*"
 
 echo ""
 echo "done!"
