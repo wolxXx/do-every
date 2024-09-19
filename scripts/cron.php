4a2eac26e0be73ab56e24a75a1cf5a169fa6b4fd
wolxXx <devops@wolxXx.de>
2024-09-19 07:54:18 +0200

    Add constants and cron cleanup
    Added STARTED constant to bootstrap for script timing. Included DISABLE_DOCTRINE_TOOLS in cron script to avoid unnecessary loading and added a command to clean temporary files at the end of the cron job.

--- scripts/cron.php
+++ scripts/cron.php
@@ -1,6 +1,8 @@
 <?php
-
+defined('DISABLE_DOCTRINE_TOOLS') || define('DISABLE_DOCTRINE_TOOLS', true);
 require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 \DoEveryApp\Util\DependencyContainer::getInstance()->getLogger()->debug('start cron');
 new \DoEveryApp\Util\Cron();
+exec('rm /tmp/__CG*');
+\DoEveryApp\Util\DependencyContainer::getInstance()->getLogger()->debug('cron finished');
