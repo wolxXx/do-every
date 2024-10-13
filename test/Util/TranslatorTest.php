b1c69f162a0da31c9135e4ab0acdc2d648cede8b
wolxXx <devops@wolxXx.de>
2024-10-13 13:30:56 +0200

    Update translation tests to handle method parameters
    Previously, the translation tests did not account for methods with parameters. Now, the tests check if methods require parameters and provide a random integer if needed. This ensures more robust testing of the Translator classes.

--- test/Util/TranslatorTest.php
+++ test/Util/TranslatorTest.php
@@ -8,22 +8,37 @@ namespace DoEveryAppTest\Util;
 class TranslatorTest extends \DoEveryAppTest\TestBase
 {
     #[\PHPUnit\Framework\Attributes\DataProvider('translationsTestDataProvider')]
-    public function testNothingTranslations($method)
+    public function testNothingTranslations($method, $parameters)
     {
-        (new \DoEveryApp\Util\Translator\Nothing())->{$method}();
+        if (0 === \count($parameters)) {
+            (new \DoEveryApp\Util\Translator\Nothing())->{$method}();
+        }
+        if (0 !== \count($parameters)) {
+            (new \DoEveryApp\Util\Translator\Nothing())->{$method}(rand(0,1000));
+        }
         $this->assertTrue(true);
     }
     #[\PHPUnit\Framework\Attributes\DataProvider('translationsTestDataProvider')]
-    public function testGermanTranslations($method)
+    public function testGermanTranslations($method, $parameters)
     {
-        (new \DoEveryApp\Util\Translator\German())->{$method}();
+        if (0 === \count($parameters)) {
+            (new \DoEveryApp\Util\Translator\German())->{$method}();
+        }
+        if (0 !== \count($parameters)) {
+            (new \DoEveryApp\Util\Translator\German())->{$method}(rand(0,1000));
+        }
         $this->assertTrue(true);
     }
 
     #[\PHPUnit\Framework\Attributes\DataProvider('translationsTestDataProvider')]
-    public function testEnglishTranslations($method)
+    public function testEnglishTranslations($method, $parameters)
     {
-        (new \DoEveryApp\Util\Translator\English())->{$method}();
+        if (0 === \count($parameters)) {
+            (new \DoEveryApp\Util\Translator\English())->{$method}();
+        }
+        if (0 !== \count($parameters)) {
+            (new \DoEveryApp\Util\Translator\English())->{$method}(rand(0,1000));
+        }
         $this->assertTrue(true);
     }
 
@@ -34,7 +49,11 @@ class TranslatorTest extends \DoEveryAppTest\TestBase
         $reflection        = new \ReflectionClass(\DoEveryApp\Util\Translator::class);
         $reflectionMethods = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
         foreach ($reflectionMethods as $method) {
-            $methods[] = [$method->getName()];
+            $parameters = [];
+            foreach ($method->getParameters() as $parameter) {
+                $parameters[$parameter->getName()] = $parameter->getType();
+            }
+            $methods[] = [$method->getName(), $parameters];
         }
 
         return $methods;
