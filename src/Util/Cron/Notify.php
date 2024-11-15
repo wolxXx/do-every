f65f8686d7776eafa39e39dd52175e7d6bfd38aa
wolxXx <devops@wolxXx.de>
2024-11-15 08:39:51 +0100

    Format code consistently by adding trailing commas.
    Improved code consistency by adding trailing commas to function calls and array declarations. This enhances readability and reduces the likelihood of syntax errors in future modifications.

--- src/Util/Cron/Notify.php
+++ src/Util/Cron/Notify.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace DoEveryApp\Util\Cron;
 
@@ -21,39 +21,42 @@ class Notify
     {
         $now             = \Carbon\Carbon::now();
         $notifierLastRun = \DoEveryApp\Util\Registry::getInstance()->getNotifierLastRun();
-        $force = false;
-        $lastCron = \Carbon\Carbon::create($notifierLastRun);
-        $lastCron->addHours(23);
-        if ($lastCron->gt($now)) {
+        $force           = false;
+        $lastCron        = \Carbon\Carbon::create($notifierLastRun,);
+        $lastCron->addHours(23,);
+        if ($lastCron->gt($now,)) {
             $force = true;
         }
 
         if (true === \DoEveryApp\Util\Registry::getInstance()->isNotifierRunning()) {
-            if(true !== $force) {
+            if (true !== $force) {
                 return;
             }
         }
 
-        \DoEveryApp\Util\Registry::getInstance()->setNotifierRunning(true);
+        \DoEveryApp\Util\Registry::getInstance()->setNotifierRunning(true,);
         \DoEveryApp\Util\DependencyContainer::getInstance()->getEntityManager()->flush();
 
         $workers       = \DoEveryApp\Entity\Worker::getRepository()->findAll();
-        $this->workers = \array_filter($workers, function (\DoEveryApp\Entity\Worker $worker) use ($now) {
+        $this->workers = \array_filter($workers, function(\DoEveryApp\Entity\Worker $worker,) use ($now,) {
             if (null === $worker->getEmail() || false === $worker->doNotify()) {
                 return false;
             }
-            $lastNotification = \DoEveryApp\Entity\Notification::getRepository()->getLastForWorker($worker);
+            $lastNotification = \DoEveryApp\Entity\Notification::getRepository()->getLastForWorker($worker,);
             if (null === $lastNotification) {
                 return true;
             }
             $notificationDate = $lastNotification->getDate();
-            $date             = \Carbon\Carbon::create($notificationDate);
-            $diff             = $date->diff($now, true, ['y', 'm']);
+            $date             = \Carbon\Carbon::create($notificationDate,);
+            $diff             = $date->diff($now, true, ['y', 'm'],);
 
             return $diff->d > 0 || $diff->h > 22;
-        });
+        },);
         $tasks         = \DoEveryApp\Entity\Task::getRepository()->findAll();
-        $tasks         = \array_filter($tasks, function (\DoEveryApp\Entity\Task $task) {
+        $tasks         = \array_filter($tasks, function(\DoEveryApp\Entity\Task $task,) {
+            if (false === $task->isActive()) {
+                return false;
+            }
             if (false === $task->isNotify()) {
                 return false;
             }
@@ -67,22 +70,22 @@ class Notify
             return true === \in_array($task->getDueUnit(), [
                     \DoEveryApp\Definition\IntervalType::HOUR->value,
                     \DoEveryApp\Definition\IntervalType::MINUTE->value,
-                ]);
-        });
-        $this->tasks   = \DoEveryApp\Util\View\TaskSortByDue::sort($tasks);
+                ],);
+        },);
+        $this->tasks   = \DoEveryApp\Util\View\TaskSortByDue::sort($tasks,);
 
         foreach ($this->workers as $worker) {
-            \DoEveryApp\Util\Debugger::debug($worker->getName() . '->' . $worker->getEmail());
+            \DoEveryApp\Util\Debugger::debug($worker->getName() . '->' . $worker->getEmail(),);
         }
         foreach ($this->tasks as $task) {
-            \DoEveryApp\Util\Debugger::debug($task->getName() . '->' . $task->getDueValue() . ' ' . $task->getDueUnit() . ' ' . \DoEveryApp\Util\View\Due::getByTask($task));
+            \DoEveryApp\Util\Debugger::debug($task->getName() . '->' . $task->getDueValue() . ' ' . $task->getDueUnit() . ' ' . \DoEveryApp\Util\View\Due::getByTask($task,),);
         }
-        \DoEveryApp\Util\Debugger::debug($this->hasSomethingToDo());
-        if (0 !== \sizeof($this->tasks)) {
+        \DoEveryApp\Util\Debugger::debug($this->hasSomethingToDo(),);
+        if (0 !== \sizeof($this->tasks,)) {
             $this->notify();
         }
-        \DoEveryApp\Util\Registry::getInstance()->setNotifierRunning(false);
-        \DoEveryApp\Util\Registry::getInstance()->setNotifierLastRun(\Carbon\Carbon::now());
+        \DoEveryApp\Util\Registry::getInstance()->setNotifierRunning(false,);
+        \DoEveryApp\Util\Registry::getInstance()->setNotifierLastRun(\Carbon\Carbon::now(),);
         \DoEveryApp\Util\DependencyContainer::getInstance()->getEntityManager()->flush();
     }
 
@@ -95,15 +98,15 @@ class Notify
                 foreach ($this->tasks as $task) {
                     $tasks[]      = $task;
                     $notification = (new \DoEveryApp\Entity\Notification())
-                        ->setWorker($worker)
-                        ->setTask($task)
-                        ->setDate(\Carbon\Carbon::now())
+                        ->setWorker($worker,)
+                        ->setTask($task,)
+                        ->setDate(\Carbon\Carbon::now(),)
                     ;
-                    $notification::getRepository()->create($notification);
+                    $notification::getRepository()->create($notification,);
                 }
-                \DoEveryApp\Util\Mailing\Notify::send($worker, $tasks);
+                \DoEveryApp\Util\Mailing\Notify::send($worker, $tasks,);
             } catch (\Throwable $exception) {
-                \DoEveryApp\Util\DependencyContainer::getInstance()->getLogger()->error('notification mail failed. ' . $exception->getMessage() . \PHP_EOL . \PHP_EOL . $exception->getTraceAsString());
+                \DoEveryApp\Util\DependencyContainer::getInstance()->getLogger()->error('notification mail failed. ' . $exception->getMessage() . \PHP_EOL . \PHP_EOL . $exception->getTraceAsString(),);
             }
         }
         \DoEveryApp\Util\DependencyContainer::getInstance()
@@ -115,6 +118,6 @@ class Notify
 
     public function hasSomethingToDo(): bool
     {
-        return 0 !== \sizeof($this->tasks) && 0 !== \sizeof($this->workers);
+        return 0 !== \sizeof($this->tasks,) && 0 !== \sizeof($this->workers,);
     }
 }
