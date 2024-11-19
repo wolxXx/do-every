153acea7a4083e6ac680749df3e1e57d084a43ed
wolxXx <devops@wolxXx.de>
2024-11-19 07:53:20 +0100

    Refactor Duration class to handle larger time units
    Revised Duration class to support displaying duration in hours and days for values exceeding specific thresholds. Included improvements in formatting and handling of zero duration.

--- src/Util/View/Duration.php
+++ src/Util/View/Duration.php
@@ -1,21 +1,22 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace DoEveryApp\Util\View;
 
 class Duration
 {
-    public static function byExecution(\DoEveryApp\Entity\Execution $execution): string
+    public static function byExecution(\DoEveryApp\Entity\Execution $execution,): string
     {
-        return static::byValue($execution->getDuration());
+        return static::byValue($execution->getDuration(),);
     }
 
 
-    public static function byValue(?int $duration = null): string
+    public static function byValue(?int $duration = null,): string
     {
         switch ($duration) {
             case null:
+            case 0:
             {
                 return \DoEveryApp\Util\DependencyContainer::getInstance()->getTranslator()->noValue();
             }
@@ -39,10 +40,16 @@ class Duration
             {
                 return \DoEveryApp\Util\DependencyContainer::getInstance()->getTranslator()->fiveMinutes();
             }
-            default:
-            {
-                return $duration . ' ' . \DoEveryApp\Util\DependencyContainer::getInstance()->getTranslator()->minutes();
-            }
         }
+        if ($duration < 120) {
+            return $duration . ' ' . \DoEveryApp\Util\DependencyContainer::getInstance()->getTranslator()->minutes();
+        }
+        $duration = round($duration / 60,);
+        if ($duration < 100) {
+            return $duration . ' ' . \DoEveryApp\Util\DependencyContainer::getInstance()->getTranslator()->hours();
+        }
+        $duration = round($duration / 24,);
+
+        return $duration . ' ' . \DoEveryApp\Util\DependencyContainer::getInstance()->getTranslator()->days();
     }
 }
