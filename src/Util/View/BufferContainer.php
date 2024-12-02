932f4b43daf0956806e895a9847f7f84c3ab42a9
wolxXx <devops@wolxXx.de>
2024-12-02 09:09:58 +0100

    Add BufferContainer for managing output buffers
    Introduce the BufferContainer class to manage and retrieve buffer contents efficiently. This class provides methods to set content by registration keys and concatenate the buffer into a single output string. Integrate it with event loops to promptly clear and process buffer data.

---
+++ src/Util/View/BufferContainer.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoEveryApp\Util\View;
+
+class BufferContainer
+{
+    private array $bufferContents = [];
+
+    private int   $counter        = 0;
+
+
+    public function next(): int
+    {
+        return $this->counter++;
+    }
+
+
+    public function __toString(): string
+    {
+        return $this->get();
+    }
+
+
+    public function get(): string
+    {
+        \Revolt\EventLoop::run();
+
+        $result               = \implode(' ', $this->bufferContents);
+        $this->bufferContents = [];
+
+        return $result;
+    }
+
+
+    public function set($registration, $content): static
+    {
+        $this->bufferContents[$registration] = $content;
+
+        return $this;
+    }
+}
