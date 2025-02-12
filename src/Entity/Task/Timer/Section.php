3d7112dffc81529a78e03135addbd50d4deeeaf6
wolxXx <devops@wolxxx.de>
2025-02-12 09:26:07 +0100

    Add Task Timer Section entity and repository
    Introduce a new `Task\Timer\Section` entity with corresponding ORM mappings for start and end timestamps, and a relationship to `Task\Timer`. Also, implement a repository for the `Task\Timer\Section` entity, extending Doctrine's capabilities with custom create, update, and delete methods. Enhance `Task\Timer` by adding a duration property to support time tracking.

--- src/Entity/Task/Timer/Section.php
+++ src/Entity/Task/Timer/Section.php
@@ -3,10 +3,10 @@
 
 declare(strict_types=1);
 
-namespace DoEveryApp\Entity\Task;
+namespace DoEveryApp\Entity\Task\Timer;
 
 #[\Doctrine\ORM\Mapping\Entity(
-    repositoryClass: Timer\Repository::class
+    repositoryClass: Section\Repository::class
 )]
 #[\Doctrine\ORM\Mapping\Table(
     name   : self::TABLE_NAME,
@@ -17,43 +17,39 @@ namespace DoEveryApp\Entity\Task;
     ],
 )]
 
-class Timer
+class Section
 {
     use \DoEveryApp\Entity\Share\Blame;
     use \DoEveryApp\Entity\Share\Id;
     use \DoEveryApp\Entity\Share\Repository;
     use \DoEveryApp\Entity\Share\Timestamp;
 
-    public const string TABLE_NAME = 'task_timer';
+    public const string TABLE_NAME = 'task_timer_section';
 
 
     #[\Doctrine\ORM\Mapping\ManyToOne(
-        targetEntity: \DoEveryApp\Entity\Task::class
+        targetEntity: \DoEveryApp\Entity\Task\Timer::class
     )]
     #[\Doctrine\ORM\Mapping\JoinColumn(
         nullable: false,
         onDelete: 'CASCADE'
     )]
-    protected \DoEveryApp\Entity\Task $task;
-
-
-    #[\Doctrine\ORM\Mapping\ManyToOne(
-        targetEntity: \DoEveryApp\Entity\Worker::class
-    )]
-    #[\Doctrine\ORM\Mapping\JoinColumn(
-        nullable: false,
-        onDelete: 'CASCADE'
-    )]
-    protected \DoEveryApp\Entity\Worker $worker;
+    protected \DoEveryApp\Entity\Task\Timer $task;
 
 
     #[\Doctrine\ORM\Mapping\Column(
-        name    : 'duration',
-        type    : \Doctrine\DBAL\Types\Types::INTEGER,
-        nullable: true
+        name    : 'start',
+        type    : \Doctrine\DBAL\Types\Types::DATETIME_MUTABLE,
+        nullable: false
     )]
-    protected ?int       $duration = null;
+    protected \DateTime  $start;
 
+    #[\Doctrine\ORM\Mapping\Column(
+        name    : 'end',
+        type    : \Doctrine\DBAL\Types\Types::DATETIME_MUTABLE,
+        nullable: false
+    )]
+    protected \DateTime  $end;
 
     public static function getRepository(): Timer\Repository
     {
