0deb3dae9d0f24278be9a8d20552b8ee77f1becc
wolxXx <devops@wolxxx.de>
2025-02-19 21:52:21 +0100

    Disable opcache setting rather than removing it.
    Commented out the opcache.enable setting instead of deleting it to preserve context and maintain flexibility for future re-enablement. This change ensures the configuration remains clear and adjustable without fully discarding relevant code.

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ ini_set(option: 'opcache.interned_strings_buffer', value: 8);
 ini_set(option: 'opcache.max_accelerated_files', value: 4000);
 ini_set(option: 'opcache.revalidate_freq', value: 60);
 ini_set(option: 'opcache.enable_cli', value: 1);
-ini_set('opcache.enable', 0);
+#ini_set(option: 'opcache.enable', value: 0);
 ini_set(option: 'opcache.max_file_size', value: 1000);
 ini_set(option: 'opcache.file_cache_only', value: 1);
 ini_set(option: 'opcache.file_cache', value: ROOT_DIR . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . '.opcache');
