b3f442a9e869495e88179bcbffd5fa2f23cc79a7
wolxXx <devops@wolxxx.de>
2025-07-23 10:27:09 +0200

    Validate required environment variables in `run.sh`
    - Added a check in `run.sh` to ensure all necessary environment variables are set before starting the application.
    - Updated `.env.dist` to include `COMPOSE_PROJECT_NAME`.

--- run.sh
+++ run.sh
@@ -7,6 +7,21 @@ cd "$HERE";
 
 source .env
 
+
+required_vars=("INSTANCE" "COMPOSE_PROJECT_NAME" "DB_PORT" "WEB_PORT")
+missing_vars=()
+
+for var_name in "${required_vars[@]}"; do
+    if [ -z "${!var_name}" ]; then
+        missing_vars+=("$var_name")
+    fi
+done
+
+if [ ${#missing_vars[@]} -ne 0 ]; then
+    echo "Error: The following variables are not set in .env file: ${missing_vars[*]}"
+    exit 1
+fi
+
 docker compose up -d --build --force-recreate --pull always
 
 echo "running under http://localhost:$WEB_PORT"
