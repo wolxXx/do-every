513e3f06a9f9246faac813f0f0e1695a38bdff39
René Thiemann <rene.thiemann@immoware24.de>
2025-09-16 14:03:43 +0200

    Simplify `CalendarAction` by removing unused SabreDAV server code and refactoring VCalendar generation.

--- src/Action/Task/CalendarAction.php
+++ src/Action/Task/CalendarAction.php
@@ -4,22 +4,16 @@ declare(strict_types=1);
 
 namespace DoEveryApp\Action\Task;
 
-use
-    Sabre\DAV,
-    Sabre\CalDAV,
-    Sabre\DAVACL;
-
-
 #[\DoEveryApp\Attribute\Action\Route(
-    path   : '/calendar',
-    methods: [
+    path        : '/calendar',
+    methods     : [
         \Fig\Http\Message\RequestMethodInterface::METHOD_GET,
         \Fig\Http\Message\RequestMethodInterface::METHOD_POST,
         \Fig\Http\Message\RequestMethodInterface::METHOD_HEAD,
         \Fig\Http\Message\RequestMethodInterface::METHOD_OPTIONS,
         \Fig\Http\Message\RequestMethodInterface::METHOD_TRACE,
         \Fig\Http\Message\RequestMethodInterface::METHOD_PUT,
-        'PROPFIND',
+        'PROPFIND',// lol
     ],
     authRequired: false,
 )]
@@ -30,14 +24,11 @@ class CalendarAction extends \DoEveryApp\Action\AbstractAction
     public function run(): \Psr\Http\Message\ResponseInterface
     {
         \DoEveryApp\Util\QueryLogger::$disabled = true;
-
         \DoEveryApp\Util\DependencyContainer::getInstance()->getLogger()->debug('CalendarAction');
         \DoEveryApp\Util\DependencyContainer::getInstance()->getLogger()->debug(\Safe\json_encode($this->getRequest()->getHeaders(), JSON_PRETTY_PRINT));
         \DoEveryApp\Util\DependencyContainer::getInstance()->getLogger()->debug(\Safe\json_encode($this->getRequest()->getParsedBody(), JSON_PRETTY_PRINT));
 
-
-        // ==== Authentifizierung prüfen ====
-        if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        if (false === isset($_SERVER['PHP_AUTH_USER'])) {
             header('WWW-Authenticate: Basic realm="Kalender"');
             header('HTTP/1.0 401 Unauthorized');
             echo 'Authentifizierung erforderlich';
@@ -50,100 +41,19 @@ class CalendarAction extends \DoEveryApp\Action\AbstractAction
             exit;
         }
 
-        // ==== Kalender erzeugen ====
-        $vcalendar = new \Sabre\VObject\Component\VCalendar();
-
+        $calendar = new \Sabre\VObject\Component\VCalendar();
         foreach (\DoEveryApp\Entity\Task::getRepository()->findAllForIndex() as $task) {
-            $due = $task->getDueValue();
-            $start = new \DateTime(datetime: 'now', timezone: new \DateTimeZone('Europe/Berlin'));
-            if ($due !== null && $due > 0) {
-                $start->modify('+'.$due.' '.$task->getIntervalType().'s');
+            if (false === $task->isActive()) {
+                continue;
             }
-            \DoEveryApp\Util\Debugger::dieDebug($due, $start->format('Y-m-d H:i:s'), $task->getIntervalType());
-
-            $end   = (clone $start)->modify('+1 hour');
-
-            $vcalendar->add('VEVENT', [
-                'UID' => $task->getId().'@do-every',
-                'SUMMARY' => $task->getName(),
-                'DTSTART' => $start,
-                'DTEND'   => $end,
-            ]);
+            $calendar->add('VEVENT', $task->toVCalendarEvent());
         }
 
-
-        // ==== Ausgabe ====
         header('Content-Type: text/calendar; charset=utf-8');
         header('Content-Disposition: inline; filename="kalender.ics"');
 
-        echo $vcalendar->serialize();
+        echo $calendar->serialize();
 
         die();
-
-
-
-        $pdo = new \PDO('mysql:host=mysql;dbname=do_every', 'root', 'root');
-        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-
-        //Mapping PHP errors to exceptions
-
-        set_error_handler(function($errno, $errstr, $errfile, $errline ) {
-            throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
-        });
-
-
-        // Backends
-        $authBackend = new DAV\Auth\Backend\PDO($pdo);
-        $principalBackend = new DAVACL\PrincipalBackend\PDO($pdo);
-        $calendarBackend = new CalDAV\Backend\PDO($pdo);
-
-        // Directory tree
-        $tree = array(
-            new DAVACL\PrincipalCollection($principalBackend),
-            new CalDAV\CalendarRoot($principalBackend, $calendarBackend),
-        );
-
-
-        // The object tree needs in turn to be passed to the server class
-        $server = new DAV\Server($tree);
-
-        // You are highly encouraged to set your WebDAV server base url. Without it,
-        // SabreDAV will guess, but the guess is not always correct. Putting the
-        // server on the root of the domain will improve compatibility.
-        $server->setBaseUri('/calendar');
-
-        // Authentication plugin
-        $authPlugin = new DAV\Auth\Plugin($authBackend,'SabreDAV');
-        /*        $server->addPlugin($authPlugin);*/
-        $authPlugin = new DAV\Auth\Plugin(new DAV\Auth\Backend\BasicCallBack(callBack: function($username, $password) {
-            $requiredUsername = \DoEveryApp\Util\Registry::getInstance()->getDavUser();
-            $requiredPassword = \DoEveryApp\Util\Registry::getInstance()->getDavPassword();
-            if (null === $requiredUsername || null === $requiredPassword) {
-                return false;
-            }
-            return (
-                $username === $requiredUsername &&
-                $password === $requiredPassword
-            );
-        }));
-        $server->addPlugin(plugin: $authPlugin);
-
-        // CalDAV plugin
-        $caldavPlugin = new CalDAV\Plugin();
-        $server->addPlugin($caldavPlugin);
-/*
-
-        // ACL plugin
-        $aclPlugin = new DAVACL\Plugin();
-        $server->addPlugin($aclPlugin);
-
-        // Support for html frontend
-        $browser = new DAV\Browser\Plugin();
-        $server->addPlugin($browser);*/
-
-        // And off we go!
-        $server->start();
-
-        \DoEveryApp\Util\Debugger::dieDebug('omfg');
     }
 }
