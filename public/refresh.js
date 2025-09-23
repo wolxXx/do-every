be81837d0000f189291d1b3d56ee27b2a66164f2
wolxXx <devops@wolxXx.de>
2025-09-23 09:23:01 +0200

    Add `refresh.js` to dynamically update elements without page reload
    - Introduced polling functionality to periodically fetch and update DOM elements.
    - Highlighted elements with missing IDs for easier debugging.

---
+++ public/refresh.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const currentUrl = window.location.href;
+
+    let timeOut = 5000
+    if (typeof local_timeout !== 'undefined') {
+        timeOut = local_timeout
+    }
+
+    let elements = document.querySelectorAll('.replaceMe')
+    let replaces = []
+    elements.forEach(element => {
+        let id = element.id;
+        if ('' === String(id).trim()) {
+            element.style.border = '2px solid red';
+            element.style.background = 'red';
+            element.style.color = 'blue';
+            return
+        }
+
+        replaces.push(id)
+    })
+    if (0 === replaces.length){
+        return
+    }
+    setInterval(function () {
+        fetch(currentUrl, {
+            method: 'GET',
+        })
+            .then(response => {
+                return response.text();
+            })
+            .then(data => {
+                const parser = new DOMParser()
+                const doc = parser.parseFromString(data, "text/html")
+                replaces.forEach(id => {
+                    let replace = ''
+                    if (doc.getElementById(id)) {
+                        replace = doc.getElementById(id).innerHTML
+                    }
+                    document.getElementById(id).innerHTML = replace
+                })
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    }, timeOut);
+});
