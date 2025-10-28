4a19cd3c74b40b046422a087f89500e566b3c6b9
wolxXx <devops@wolxXx.de>
2025-10-28 17:57:18 +0100

    Refactor `refresh.js` for readability and modularity
    - Extracted repetitive logic into a reusable `fetchPage` function.
    - Simplified DOMContentLoaded block and improved overall structure.

--- public/refresh.js
+++ public/refresh.js
@@ -1,14 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const currentUrl = window.location.href;
 
+let replaces = []
+const currentUrl = window.location.href;
+
+function fetchPage() {
+    fetch(currentUrl, {
+        method: 'GET',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    })
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            const parser = new DOMParser()
+            const doc = parser.parseFromString(data, "text/html")
+            replaces.forEach(id => {
+                let replace = ''
+                document.getElementById(id).innerHTML = replace
+                if (doc.getElementById(id)) {
+                    replace = doc.getElementById(id).innerHTML
+                }
+                document.getElementById(id).innerHTML = replace
+            })
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
     let timeOut = 5000
     if (typeof local_timeout !== 'undefined') {
         timeOut = local_timeout
     }
 
-    let elements = document.querySelectorAll('.replaceMe')
-    let replaces = []
-    elements.forEach(element => {
+    document.querySelectorAll('.replaceMe').forEach(element => {
         let id = element.id;
         if ('' === String(id).trim()) {
             element.style.border = '2px solid red';
@@ -22,29 +49,9 @@ document.addEventListener('DOMContentLoaded', function () {
     if (0 === replaces.length){
         return
     }
-    setInterval(function () {
-        fetch(currentUrl, {
-            method: 'GET',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            }
-        })
-            .then(response => {
-                return response.text();
-            })
-            .then(data => {
-                const parser = new DOMParser()
-                const doc = parser.parseFromString(data, "text/html")
-                replaces.forEach(id => {
-                    let replace = ''
-                    if (doc.getElementById(id)) {
-                        replace = doc.getElementById(id).innerHTML
-                    }
-                    document.getElementById(id).innerHTML = replace
-                })
-            })
-            .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-            });
-    }, timeOut);
+    fetchPage()
+    setInterval(() => {
+        fetchPage()
+    }, timeOut)
+
 });
