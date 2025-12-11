a617c563e5011e9881ab423a96174f65315fecfd
wolxXx <devops@wolxXx.de>
2025-12-11 08:52:54 +0100

    Refactor `BaseTest` to update assertions, improve test readability, and introduce `Html2Text` for response processing.

--- test/Views/BaseTest.php
+++ test/Views/BaseTest.php
@@ -13,25 +13,35 @@ class BaseTest extends ViewTestBase
             templateName: 'action/cms/index.php',
         );
 
-        $this->assertTrue(condition: \str_contains(haystack: $result->responseBody, needle: '<img src="/bee.png" alt="logo" style="max-width: 100px; max-height: 100px;">'));
+        $this->assertSame(\PHP_EOL.'hallo!', $result->responseBody);
     }
     public function testDashboard()
     {
+        $registry = \DoEveryApp\Util\Registry::getInstance()->setDoFillTimeLine(fillTimeLine: true);
+        $this->assertTrue(\DoEveryApp\Util\Registry::getInstance()->doFillTimeLine());
+        \Carbon\Carbon::setTestNow('2010-01-01 00:00:00');
+
         $result = $this->render(
             templateName: 'action/cms/dashboard.php',
             data        : [
-                              'tasks' => [],
-                              'disabledTasks' => [],
-                              'workingOn' => [],
-                              'workers' => [],
-                              'groups' => [],
+                              'tasks'             => [],
+                              'disabledTasks'     => [],
+                              'workingOn'         => [],
+                              'workers'           => [],
+                              'groups'            => [],
                               'tasksWithoutGroup' => [],
-                              'dueTasks' => [],
-                              'executions' => [],
+                              'dueTasks'          => [],
+                              'executions'        => [],
+                              'currentUser'       => $this->getDefaultUser(),
                           ],
             worker      : $this->getDefaultUser()
         );
 
-        $this->assertStringContainsString(needle: '<img src="/bee.png" alt="logo" style="max-width: 100px; max-height: 100px;">', haystack: $result->responseBody);
+
+        $html2Text = new \Html2Text\Html2Text();
+        $html2Text->setHtml($result->responseBody);
+        $text      = $html2Text->getText();
+
+        $this->assertStringContainsString(needle: 'DASHBOARD', haystack: $text);
     }
 }
